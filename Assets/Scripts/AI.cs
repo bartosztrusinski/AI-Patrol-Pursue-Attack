62ce1ace2c62c486d15f5dbb02372c19cc2e2f7f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-06-29 02:55:07 +0200

    setup project

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+  NavMeshAgent agent;
+  Animator anim;
+  State currentState;
+
+  public Transform player;
+
+  void Start()
+  {
+    agent = GetComponent<NavMeshAgent>();
+    anim = GetComponent<Animator>();
+    currentState = new Idle(gameObject, agent, anim, player);
+  }
+
+
+  void Update()
+  {
+    currentState = currentState.Process();
+  }
+}
